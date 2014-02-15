4f257a90dad13718b20732db89142e6e61ac6e76
daniel-bulger <dpbulger@gmail.com>
2014-02-15 05:47:27 -0500

    Added code to prompt user to post a message on facebook about their trump victory.  Also made debugging code in the hidden web view easier by putting it all in a try/catch :p

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,37 +1,60 @@
 function doClick(e) {
-    alert($.label.text);
+        alert($.label.text);
 }
-
+    
 $.index.open();
 
 var win = $.index;
-        
-Ti.App.addEventListener('app:gameListChanged', function(e) {
-    var games = e.games;
-    var lalala = [];
-    for (var i = 0; i < games.length; i++)
+
+Ti.App.addEventListener('app:proxyViewLoaded',function(){
+    Ti.App.addEventListener('app:gameListChanged', function(e) {
+        var games = e.games;
+        var lalala = [];
+        for (var i = 0; i < games.length; i++)
+        {
+    	    lalala.push({properties: { title: games[i]}});
+        }
+        $.gamesList.setItems(lalala);
+    });
+    
+    var fb = require('facebook');
+    fb.appid = 259836610857049;
+    fb.permissions = ['email', 'user_likes', 'publish_stream'];
+    fb.forceDialogAuth = true;
+    
+    fb.addEventListener('login', function(e) {
+        if (e.success) {
+        	Ti.App.fireEvent('app:fbAuthed', {access_token: fb.getAccessToken()});
+        } else if (e.error) {
+            alert(e.error);
+        } else if (e.cancelled) {
+    	    alert("You don't want to give permission? :('");
+        }
+    });
+    if (!fb.loggedIn) {
+        fb.authorize();
+    }
+    else
     {
-	    lalala.push({properties: { title: games[i]}});
+        Ti.App.fireEvent("app:fbAuthed",{access_token:fb.accessToken});
     }
-    $.gamesList.setItems(lalala);
+    Ti.App.addEventListener("app:playerWonRound",function(e)
+        {
+            // Now create the status message after you've confirmed that authorize() succeeded
+            fb.dialog('feed', {caption: "I just won a round of Trump with this card: ",link: e.winning_url}, 
+              function(e) {
+                if (e.success) {
+                    alert("Thank you for your support!");
+                } else {
+                    if (e.error) {
+                        alert(e.error);
+                    } else {
+                        alert("Cancelled");
+                    }
+                }
+            });    
+        });
 });
-
-var fb = require('facebook');
-fb.appid = 259836610857049;
-fb.permissions = ['email', 'user_likes'];
- 
-fb.addEventListener('login', function(e) {
-    if (e.success) {
-    	Ti.App.fireEvent('app:fbAuthed', {access_token: fb.getAccessToken()});
-    } else if (e.error) {
-        alert(e.error);
-    } else if (e.cancelled) {
-	    alert("You don't want to give permission? :('");
-    }
-});
-
-fb.authorize();
-
 if(OS_IOS)
 {
     $.webviewproxy.url = "firebase/webviewproxy.html";
