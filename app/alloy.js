4e8db3bc3ab4d3124dc8e29e2216a4261d0a0565
Derek Schultz <derek@derekschultz.net>
2014-02-14 21:47:25 -0500

    Adds a tiny bit of styling

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,18 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+if (OS_IOS || OS_ANDROID) {
+	Alloy.Globals.top = 0;
+	Alloy.Globals.tableTop = '50dp';
+
+	try {
+		// check for iOS7
+		if (OS_IOS && parseInt(Titanium.Platform.version.split(".")[0], 10) >= 7) {
+			Alloy.Globals.top = '20dp';
+			Alloy.Globals.tableTop = '70dp';
+		}
+	} catch(e) {
+		// catch and ignore
+	}
+}
