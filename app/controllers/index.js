86557b64be4ab6c03b298a9830fee71e39a667f7
daniel-bulger <dpbulger@gmail.com>
2014-02-15 14:51:26 -0500

    Added code to create firebase user account, and to post on your wall

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,19 +6,50 @@ $.index.open();
 
 var win = $.index;
 Ti.App.addEventListener('app:proxyViewLoaded',function(){
-    // this code will execute when the invisible web frame has loaded
-    Ti.App.addEventListener('app:gameListChanged', function(e) {
-        var games = e.games;
-        var lalala = [];
-        for (var i = 0; i < games.length; i++)
-        {
-    	    lalala.push({properties: { title: games[i]}});
-        }
-    });
     // load the facebook module
     var fb = require('facebook');
     fb.appid = 259836610857049;
     fb.permissions = ['email', 'user_likes'];
+    
+    fb.addEventListener('login', function(e) {
+        if (e.success) {
+            Ti.App.fireEvent('app:fbAuthed', {access_token: fb.getAccessToken()});
+        } else if (e.error) {
+            alert(e.error);
+        } else if (e.cancelled) {
+            var dialog = Ti.UI.createAlertDialog({
+                message: 'Sorry, you must connect to facebook in order to play Trump',
+                ok: 'Okay',
+                title: 'Sign-in required'
+                });
+            dialog.addEventListener("click",function(e){fb.authorize();});
+            dialog.show();
+        }
+    });
+    
+    Ti.App.addEventListener("app:fbAuthed",function(e){
+        fb.requestWithGraphPath("me/friends",{access_token:fb.getAccessToken()},"GET",function(e){
+            var trump_friends = [];
+            // e.result.forEach(function(friend){
+//                                
+            // });
+        });
+    });
+    Ti.App.addEventListener("app:playerWonRound",function(e){
+        // Now create the status message after you've confirmed that authorize() succeeded
+        fb.dialog('feed', {caption: "I just won a round of Trump with this card: ",link: e.winning_url}, 
+          function(e) {
+            if (e.success) {
+                alert("Thank you for your support!");
+            } else {
+                if (e.error) {
+                    alert(e.error);
+                } else {
+                    alert("Cancelled");
+                }
+            }
+        });    
+    });
     
     // check to see if our auth token is valid.  Seriously, this is the way to do it...
     // https://developers.facebook.com/blog/post/2011/05/13/how-to--handle-expired-access-tokens/
@@ -31,22 +62,6 @@ Ti.App.addEventListener('app:proxyViewLoaded',function(){
             fb.logout();
             fb.authorize();
         }
-        // at this point we are guaranteed to be authenticated with facebook
-        fb.addEventListener('login', function(e) {
-            if (e.success) {
-                Ti.App.fireEvent('app:fbAuthed', {access_token: fb.getAccessToken()});
-            } else if (e.error) {
-                alert(e.error);
-            } else if (e.cancelled) {
-                var dialog = Ti.UI.createAlertDialog({
-                    message: 'Sorry, you must connect to facebook in order to play Trump',
-                    ok: 'Okay',
-                    title: 'Sign-in required'
-                    });
-                dialog.addEventListener("click",function(e){fb.authorize();});
-                dialog.show();
-            }
-        });
         if (!fb.loggedIn) {
             fb.authorize();
         }
@@ -54,22 +69,7 @@ Ti.App.addEventListener('app:proxyViewLoaded',function(){
         {
             Ti.App.fireEvent("app:fbAuthed",{access_token:fb.accessToken});
         }
-        Ti.App.addEventListener("app:playerWonRound",function(e)
-            {
-                // Now create the status message after you've confirmed that authorize() succeeded
-                fb.dialog('feed', {caption: "I just won a round of Trump with this card: ",link: e.winning_url}, 
-                  function(e) {
-                    if (e.success) {
-                        alert("Thank you for your support!");
-                    } else {
-                        if (e.error) {
-                            alert(e.error);
-                        } else {
-                            alert("Cancelled");
-                        }
-                    }
-                });    
-            });
+
     });
 
 });
