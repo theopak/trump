f339678d7929d1742978b9aa48877a77afceadc0
Theodore X. Pak <mail@theopak.com>
2014-02-16 07:12:42 -0500

    work

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,7 +7,7 @@
 //});
 
 //var win = $.index;
-Alloy.createController('play');
+//Alloy.createController('play');
 
 var emptyStateView = Ti.UI.createView({
    	top: 300,		// Initialized far away and animated into place to 15
@@ -279,7 +279,7 @@ Ti.App.addEventListener('app:gameListChanged', function(e) {
 	}
 	
 	$.main.add(listView);
-	//Alloy.createController('play').getView().open();
+	//Alloy.createController('play', {game: games[0]}).getView().open();
 	//Ti.App.fireEvent('app:play', {game: games[0]});
 	
 });
@@ -288,9 +288,14 @@ Ti.App.addEventListener('app:gameListChanged', function(e) {
 	// Changes the item template rather than the list item's color property
 	// http://docs.appcelerator.com/titanium/latest/#!/api/Titanium.UI.ListItem
 	function playMatch(e) {
-		Ti.API.info("playMatch() triggered.");
+		Ti.API.info("playMatch() triggered: " + e);
     	var item = gamesList.getItemAt(e.itemIndex);
-    	Ti.App.fireEvent('app:play', {game: e.source.game_id});
+    	//Ti.App.fireEvent('app:play', {game: e.source.game_id});
+    	//Alloy.createController('play', {game: e.source.game_id}).getView().open();
+    	var demoGame = [];
+    	demoGame.adjective = "Demonstable";
+    	demoGame.judge = {name: "Derek", id: 0};
+    	Alloy.createController('play', {game: demoGame}).getView().open();
 	}
 
 Ti.App.addEventListener('app:webviewproxyDidLoad', function (e) {
--- app/controllers/play.js
+++ app/controllers/play.js
@@ -1,70 +1,66 @@
-Ti.App.addEventListener('app:play', function(game) {
-	try {
-	Ti.API.info("Event Fired: app:play."+game.game);
-	Ti.API.info("woo");
-	Ti.App.addEventListener('app:gameInfoAcquired', function(gamedata) {
-		Ti.API.info("hiii");
-		var game = gamedata;
-		$.play.open();
-		var gameView = Ti.UI.createView({
-		   	top: 300,		// Initialized far away and animated into place to 15
-		   	opacity: 0,		// Initialized transparent and animated to fully opaque state
-			left: 10,
-			right: 10,
-		   	height: 110,
-			backgroundColor: "transparent",
-		});
-		var gameViewJudge = Ti.UI.createLabel({
-			text: game.judge.name + " started a game about",
-			//text: "Derek started a game about",
-			top: 15,
-			left: 20,
-			right: 20,
-		   	color: "black",
-		   	textAlign: "center",
-		   	font: {
-				fontFamily: "Omnes-Regular",
-				fontSize: 18
-			},
-		});
-		var gameViewTopic = Ti.UI.createLabel({
-			text: game.adjective,
-			//text: "BORING",
-			top: 45,
-			left: 20,
-			right: 20,
-			textAlign: "left",
-			   color: "black",
-			   textAlign: "center",
-			font: {
-				fontFamily: "Proxima Nova-Extra Bold",
-				fontSize: 56
-			},
-		});
-		var gameViewPicture = Ti.UI.createImageView({
-			image: "http://graph.facebook.com/"+gamedata.judge.id+"/picture?width=200&height=200",
-			borderRadius: 100,
-			width: 200,
-			height: 200
-		});
-		
-		gameView.add(gameViewJudge);
-		gameView.add(gameViewTopic);
-		gameView.add(gameViewPicture);
-		Ti.API.info("Displaying...");
-		$.play.add(gameView);
-		gameView.animate({
-		     	top: 80,
-		     	height: 400,
-		     	opacity: 1,
-		     	duration: 350,
-		     	curve: Ti.UI.ANIMATION_CURVE_EASE_OUT,
-		});
+var game = arguments[0] || {};
+Ti.API.info('here');
+//try {
+	Ti.API.info("play.js: " + game.adjective.toString());
+	//$.play.open();
+	var gameView = Ti.UI.createView({
+	   	top: 300,		// Initialized far away and animated into place to 15
+	   	opacity: 0,		// Initialized transparent and animated to fully opaque state
+		left: 10,
+		right: 10,
+	   	height: 110,
+		backgroundColor: "transparent",
 	});
-	Ti.App.fireEvent('app:requestGameInfo', {game: game.game});
-	} catch (e) {alert(e);}
+	var gameViewJudge = Ti.UI.createLabel({
+		text: game.judge.name + " is the Judge",
+		top: 15,
+		left: 20,
+		right: 20,
+	   	color: "black",
+	   	textAlign: "center",
+	   	backgroundColor: "white",
+	   	font: {
+			fontFamily: "Omnes-Regular",
+			fontSize: 18
+		},
+	});
+	var gameViewTopic = Ti.UI.createLabel({
+		text: game.adjective,
+		//text: "BORING",
+		top: 45,
+		left: 20,
+		right: 20,
+		textAlign: "left",
+		   color: "black",
+		   textAlign: "center",
+		font: {
+			fontFamily: "Proxima Nova-Extra Bold",
+			fontSize: 56
+		},
+	});
+	var gameViewPicture = Ti.UI.createImageView({
+		image: "http://graph.facebook.com/" + gamedata.judge.id + "/picture?width=200&height=200",
+		borderRadius: 100,
+		width: 200,
+		height: 200
+	});
+	
+	gameView.add(gameViewJudge);
+	gameView.add(gameViewTopic);
+	gameView.add(gameViewPicture);
+	Ti.API.info("Displaying...");
+	$.play.add(gameView);
+	gameView.animate({
+	     top: 80,
+	     height: 400,
+	     opacity: 1,
+	     duration: 350,
+	     curve: Ti.UI.ANIMATION_CURVE_EASE_OUT,
+	});
 
-});
+	//Ti.App.fireEvent('app:requestGameInfo', {game: game.game});
+//} catch (e) {alert(e);}
+
 
 
 // Handle touch events etc.
