5ebad9e97f5f60b8b25b33869cae338c99a31e9e
Theodore X. Pak <mail@theopak.com>
2014-02-15 21:55:38 -0500

    merge etc.

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -14,6 +14,15 @@ btnBack.addEventListener('click', function(){
 });
 */
 
+$.submit.addEventListener("touchstart",  function(){$.submit.backgroundColor = "#9CC075";});
+//$.submit.addEventListener("touchend",    function(){$.submit.backgroundColor = "#7C9A5B";});
+$.submit.addEventListener("touchcancel", function(){$.submit.backgroundColor = "#7C9A5B";});
+
+$.submit.addEventListener("touchend", function(e){
+	Ti.API.info("Pressed button: Invite.");
+	$.submit.backgroundColor = "#7C9A5B";	
+	Ti.App.fireEvent('app:createGame', {friends: $.friends.value.split(',')});
+});
 
 // (?): https://developer.appcelerator.com/question/27291/button-inside-a-view---inside-a-tableviewrow
 // (?): http://developer.appcelerator.com/question/124202/how-to-add-a-text-and-a-checkbox-on-a-tableviewrow
