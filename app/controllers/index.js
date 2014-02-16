5c639610793df070970c17019ad7cc2a5cfa3005
Derek Schultz <derek@derekschultz.net>
2014-02-16 06:03:37 -0500

    Some polish tweaks

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -205,7 +205,8 @@ var myTemplate = {
         	bindId: 'arrow',
         	properties: {
         		image: 'line.png',
-        		right: 10
+        		right: 10,
+        		height: 20
         	}
         }
     ],
